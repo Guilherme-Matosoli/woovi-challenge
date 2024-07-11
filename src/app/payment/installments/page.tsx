a3743143fa58077c9b9d36161f8f311ad4ead741
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-10 21:47:41 -0300

    initialize installments page :rocket:

---
+++ src/app/payment/installments/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+export default function Installments() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.mainText}>
+        João, como você quer pagar?
+      </h2>
+    </div>
+  )
+}
