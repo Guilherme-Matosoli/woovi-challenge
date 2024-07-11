6b2c1200105199d8ff1ff7959a0935f030896b8c
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-11 16:54:29 -0300

    initialize payment page :robot:

---
+++ src/pages/payment/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+export default function Payment() {
+  return (
+    <main className={styles.container}>
+      <h2 className={styles.mainText}>
+        João, pague a entrada de R$ 15.300,00 pelo Pix
+      </h2>
+    </main>
+  )
+}
