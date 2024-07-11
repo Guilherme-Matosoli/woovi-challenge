5d550ff4ebd7ba9edcd536185493ada3c112b247
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-11 16:49:28 -0300

    create an route to payment page :rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import Installments from "./pages/installments/page"
+import Installments from "./pages/installments/"
 import RootLayout from "./layout"
+import Payment from "./pages/payment"
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<RootLayout />} >
           <Route index element={<Installments />} />
+          <Route path="/payment/installments" element={<Installments />} />
+          <Route path="/payment/pay" element={<Payment />} />
         </Route>
       </Routes>
     </Router>
