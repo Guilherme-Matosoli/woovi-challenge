0f8df4b218b794418e01c24ad72826055455723c
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-11 20:14:08 -0300

    implements Payment context on pages that need it :rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Installments from "./pages/installments/"
 import RootLayout from "./layout"
 import Payment from "./pages/payment"
+import { PaymentContextProvider } from "./contexts/PaymentContext"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<RootLayout />} >
-          <Route index element={<Installments />} />
-          <Route path="/payment/installments" element={<Installments />} />
-          <Route path="/payment/pay" element={<Payment />} />
+          <PaymentContextProvider>          <Route index element={<Installments />} />
+            <Route path="/payment/installments" element={<Installments />} />
+            <Route path="/payment/pay" element={<Payment />} />
+          </PaymentContextProvider>
         </Route>
       </Routes>
     </Router>
