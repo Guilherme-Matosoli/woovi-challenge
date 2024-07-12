a26fa6a5172bd07ee8a6e79fa5b8dd720e706f6c
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-12 10:14:58 -0300

    add installmente, setInstallment and total value in payment context :rocket:

--- src/contexts/PaymentContext.tsx
+++ src/contexts/PaymentContext.tsx
@@ -1,14 +1,36 @@
-import { createContext } from "react";
+import { createContext, Dispatch, SetStateAction, useMemo, useState } from "react";
+import { currencyFormatter } from "../utils/currencyFormater";
 
 interface PaymentContextProviderProps {
   children: React.ReactNode
 }
 
-const PaymentContext = createContext({});
+interface Installment {
+  quantity: number,
+  value: number
+}
+
+interface PaymentContextProps {
+  installment: Installment | undefined,
+  setInstallment: Dispatch<SetStateAction<Installment | undefined>>,
+
+  total: string
+};
+
+export const PaymentContext = createContext({} as PaymentContextProps);
 
 export const PaymentContextProvider = ({ children }: PaymentContextProviderProps) => {
+  const [installment, setInstallment] = useState<Installment>();
+
+  const total = useMemo(() => {
+    const valueDivided = installment?.value! / 100;
+
+    const formatedTotal = currencyFormatter.format(valueDivided * installment?.quantity!);
+    return formatedTotal;
+  }, [installment]);
+
   return (
-    <PaymentContext.Provider value={{}}>
+    <PaymentContext.Provider value={{ installment, setInstallment, total }}>
       {children}
     </PaymentContext.Provider>
   );
