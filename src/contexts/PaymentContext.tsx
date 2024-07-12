9e3b7c1d43095a18de843f35bb7bdd872005f89f
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-12 10:27:53 -0300

    add pixInfo field in payment context :hammer:

--- src/contexts/PaymentContext.tsx
+++ src/contexts/PaymentContext.tsx
@@ -3,24 +3,47 @@ import { currencyFormatter } from "../utils/currencyFormater";
 
 interface PaymentContextProviderProps {
   children: React.ReactNode
-}
+};
 
 interface Installment {
   quantity: number,
   value: number
+};
+
+interface PixInfo {
+  identifier: string,
+  code: string,
+  expiresIn: Date
 }
 
 interface PaymentContextProps {
   installment: Installment | undefined,
   setInstallment: Dispatch<SetStateAction<Installment | undefined>>,
 
-  total: string
+  total: string,
+  installmentValue: string,
+
+  pixInfo: PixInfo,
+  setPixInfo: Dispatch<SetStateAction<PixInfo>>
 };
+
 
 export const PaymentContext = createContext({} as PaymentContextProps);
 
 export const PaymentContextProvider = ({ children }: PaymentContextProviderProps) => {
   const [installment, setInstallment] = useState<Installment>();
+  const [pixInfo, setPixInfo] = useState<PixInfo>({
+    identifier: "2c1b951f356c4680b13ba1c9fc889c47",
+    code: "JHGASJDGKJHGJUH3I2U4632847JKSHDFABI7Y6T342UYGJKDHSFAGVF87T34GHJVHJ",
+    expiresIn: new Date()
+  });
+
+  const installmentValue = useMemo(() => {
+    const valueDivided = installment?.value! / 100;
+
+    const formatedValue = currencyFormatter.format(valueDivided);
+    return formatedValue
+  }, [installment]);
 
   const total = useMemo(() => {
     const valueDivided = installment?.value! / 100;
@@ -30,7 +53,16 @@ export const PaymentContextProvider = ({ children }: PaymentContextProviderProps
   }, [installment]);
 
   return (
-    <PaymentContext.Provider value={{ installment, setInstallment, total }}>
+    <PaymentContext.Provider value={{
+      installment,
+      setInstallment,
+
+      total,
+      installmentValue,
+
+      pixInfo,
+      setPixInfo
+    }}>
       {children}
     </PaymentContext.Provider>
   );
