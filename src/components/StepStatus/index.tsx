6dbc91a469c6565de8c499038b82c942a4540b0b
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-12 13:47:24 -0300

    differ between actual payment and nexts :rocket:

--- src/components/StepStatus/index.tsx
+++ src/components/StepStatus/index.tsx
@@ -1,13 +1,14 @@
 import styles from "./styles.module.css";
 
 interface StepStatusProps {
-  haveNext?: boolean,
-  checked?: boolean
+  haveNext: boolean,
+  checked: boolean,
+  actual: boolean
 }
 
-export function StepStatus({ haveNext, checked }: StepStatusProps) {
+export function StepStatus({ haveNext, checked, actual }: StepStatusProps) {
   return (
-    <div className={`${styles.container} ${haveNext && styles.next} ${checked && styles.checked}`}>
+    <div className={`${styles.container} ${actual && styles.actual} ${haveNext && styles.next} ${checked && styles.checked}`}>
       {
         checked && <img src="/checked-icon.svg" alt="Etapa concluída" />
       }
