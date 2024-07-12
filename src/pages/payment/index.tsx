946658c21336b798105b2f863b712403500b286c
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-12 19:22:50 -0300

    remove static user name and replace for the PaymentContext user informations :wrench:

--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -8,25 +8,31 @@ import { Pix } from "./pix";
 import { CreditCard } from "./creditCard";
 
 export default function Payment() {
-  const { installmentValue, installment, total, pixInfo, paymentSteps } = useContext(PaymentContext);
+  const {
+    installmentValue,
+    installment,
+    total,
+    pixInfo,
+    paymentSteps,
+    clientInfo } = useContext(PaymentContext);
 
   return (
     <main className={styles.container}>
       <h2 className={styles.mainText}>
         {
-          paymentSteps == 1 && installment?.quantity == 1 && `João, pague o valor de ${installmentValue} no pix`
+          paymentSteps == 1 && installment?.quantity == 1 && `${clientInfo.name}, pague o valor de ${installmentValue} no pix`
         }
 
         {
-          paymentSteps == 1 && installment?.quantity! > 1 && `João, pague a entrada de ${installmentValue} no pix`
+          paymentSteps == 1 && installment?.quantity! > 1 && `${clientInfo.name}, pague a entrada de ${installmentValue} no pix`
         }
 
         {
-          paymentSteps > 1 && paymentSteps != installment?.quantity && `João, pague a ${paymentSteps}ª parcela em 1x no cartão`
+          paymentSteps > 1 && paymentSteps != installment?.quantity && `${clientInfo.name}, pague a ${paymentSteps}ª parcela em 1x no cartão`
         }
 
         {
-          paymentSteps == installment?.quantity && installment.quantity != 1 && `João, pague o restante em 1x no cartão`
+          paymentSteps == installment?.quantity && installment.quantity != 1 && `${clientInfo.name}, pague o restante em 1x no cartão`
         }
       </h2>
 
