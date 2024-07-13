57acdc91fc789497a1eb528d6ffede125d1ebc15
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-12 22:17:46 -0300

    add function to verify cpf validation :tada:

---
+++ src/utils/validateCpf.ts
@@ -0,0 +1,25 @@
+export const validateCpf = (cpf: string) => {
+  cpf = cpf.replace(/[^\d]/g, '');
+
+  if (/^(\d)\1*$/.test(cpf)) return false;
+
+  let sum = 0;
+  let remainder;
+
+  for (let i = 1; i <= 9; i++) {
+    sum += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+  }
+  remainder = (sum * 10) % 11;
+  if (remainder === 10 || remainder === 11) remainder = 0;
+  if (remainder !== parseInt(cpf.substring(9, 10))) return false;
+
+  sum = 0;
+  for (let i = 1; i <= 10; i++) {
+    sum += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+  }
+  remainder = (sum * 10) % 11;
+  if (remainder === 10 || remainder === 11) remainder = 0;
+  if (remainder !== parseInt(cpf.substring(10, 11))) return false;
+
+  return true;
+};
