138f8eae397c7fff42c1810b4573e0124f91f6ea
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-13 19:01:49 -0300

    initialize PixResolver :tada:

---
+++ api/src/resolvers/PixResolver.ts
@@ -0,0 +1,21 @@
+import { Arg, Query, Resolver } from "type-graphql";
+import { PixInfo } from "../models/PaymentModal";
+import Payment from "../database/entities/Payment";
+
+@Resolver()
+export class PixResolver {
+  @Query(() => PixInfo)
+  async pix(
+    @Arg("id")
+    id: string
+  ) {
+
+    try {
+      const { pixInfo } = await Payment.findOne({ id });
+      return pixInfo
+    }
+    catch (err) {
+      console.log(err);
+    };
+  }
+};
