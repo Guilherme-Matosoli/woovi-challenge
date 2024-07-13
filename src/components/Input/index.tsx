324059e57ce7802160af87300325d4225f3f11cd
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-13 11:39:28 -0300

    add error span :rocket:

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,27 +3,35 @@ import { BaseTextFieldProps, TextField } from "@mui/material"
 
 interface InputProps extends BaseTextFieldProps {
   label: string,
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  errorMessage: string | undefined
 }
 
-export function Input({ label, onChange, ...rest }: InputProps) {
+export function Input({ label, onChange, errorMessage, ...rest }: InputProps) {
   return (
-    <TextField
-      className={styles.input}
-      label={label}
-      onChange={onChange}
-      {...rest}
+    <div className={styles.container}>
+      <TextField
+        className={styles.input}
+        label={label}
+        onChange={onChange}
+        error={errorMessage ? true : false}
+        {...rest}
 
-      InputLabelProps={{
-        sx: {
-          '&.MuiInputLabel-root.MuiInputLabel-shrink': {
-            transform: 'translate(20px, -9px)',
-            fontSize: '14px',
-            background: 'white',
-            padding: '0 2px',
+        InputLabelProps={{
+          sx: {
+            '&.MuiInputLabel-root.MuiInputLabel-shrink': {
+              transform: 'translate(20px, -9px)',
+              fontSize: '14px',
+              background: 'white',
+              padding: '0 2px',
+            }
           }
-        }
-      }}
-    />
+        }}
+      />
+
+      {
+        errorMessage && <span className={styles.error}>{errorMessage}</span>
+      }
+    </div>
   )
 }
