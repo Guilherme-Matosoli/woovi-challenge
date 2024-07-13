d2b569ec6fb2da3179b124d82a5350d67264dc37
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-13 19:04:29 -0300

    create Payment with all informations :robot:

--- api/src/resolvers/PaymentResolver.ts
+++ api/src/resolvers/PaymentResolver.ts
@@ -1,11 +1,52 @@
-import { Query, Resolver } from "type-graphql";
+import crypto from "node:crypto";
 
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import Payment from "../database/entities/Payment";
+import { PaymentModel } from "../models/PaymentModal";
+import { PaymentInput } from "../inputs/PaymentInput";
+import { fakePikGenerator } from "../utils/fakePixGenerator";
 
 @Resolver()
 export class PaymentResolver {
 
-  @Query(() => String)
-  async payment() {
-    return "Payments"
-  }
+  @Query(() => PaymentModel, { nullable: true })
+  async payment(
+    @Arg("id", () => String)
+    id: string
+  ) {
+    const payment = await Payment.findOne({ id });
+
+    return payment;
+  };
+
+  @Mutation(() => PaymentModel)
+  async createPayment(
+    @Arg("input")
+    input: PaymentInput
+  ) {
+
+    const fakePix = fakePikGenerator();
+
+    const payment = await Payment.create({
+      id: crypto.randomUUID(),
+      client: {
+        name: input.name,
+        cpf: input.cpf
+      },
+      pixInfo: {
+        identifier: fakePix.identifier,
+        code: fakePix.code,
+        value: input.value
+      },
+
+      installment: {
+        quantity: input.quantity,
+        value: input.value
+      },
+      steps: 1,
+      concluded: false
+    });
+
+    return payment
+  };
 };
