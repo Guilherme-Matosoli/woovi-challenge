d5264da6c3bb1076d92e3816ff3a04fea77bd131
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-13 20:42:21 -0300

    configure apollo provider :rocket:

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './globals.css'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './services/apollo.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
