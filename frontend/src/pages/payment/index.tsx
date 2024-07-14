88e0443192b2284d7399943f88c5a51ea09e65cc
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-14 10:41:43 -0300

    get payment informations on loading :rocket:

--- frontend/src/pages/payment/index.tsx
+++ frontend/src/pages/payment/index.tsx
@@ -6,7 +6,32 @@ import styles from "./styles.module.css";
 import { PaymentContext } from "../../contexts/PaymentContext";
 import { Pix } from "./pix";
 import { CreditCard } from "./creditCard";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { socket } from "../../services/websocket";
+import { gql, useQuery } from "@apollo/client";
+import { LoadingIcon } from "../../components/LoadingIcon";
+
+const GET_PAYMENT = gql`
+  query($id: String!){
+    payment(id: $id){
+      client{
+        cpf
+        name
+      }
+      installment{
+        quantity
+        value
+      }
+      pixInfo{
+        code
+        identifier
+        expiresIn
+      }
+      steps
+      concluded
+    }
+  }
+`;
 
 export default function Payment() {
   const {
@@ -15,30 +40,55 @@ export default function Payment() {
     total,
     pixInfo,
     paymentSteps,
-    clientInfo } = useContext(PaymentContext);
+    clientInfo,
+    setPixInfo,
+    setClientInfo,
+    setInstallment,
+    setPaymentSteps
+  } = useContext(PaymentContext);
+
+  const { paymentId } = useParams();
+
+  const { loading } = useQuery(GET_PAYMENT, {
+    variables: {
+      id: paymentId
+    },
+    onCompleted: data => {
+      console.log(data)
+      setPixInfo(data.payment.pixInfo);
+      setClientInfo(data.payment.client);
+      setInstallment(data.payment.installment);
+      setPaymentSteps(data.payment.steps)
+    }
+  });
 
   const navigate = useNavigate();
   useEffect(() => {
     if (!installment) navigate('/payment/installments');
+
+    socket.emit("join", paymentId);
   }, []);
 
-  return (
+  const clientFirstName = clientInfo?.name.split(" ")[0];
+
+  return !loading && (
     <main className={styles.container}>
+      {loading && <LoadingIcon />}
       <h2 className={styles.mainText}>
         {
-          paymentSteps == 1 && installment?.quantity == 1 && `${clientInfo.name}, pague o valor de ${installmentValue} no pix`
+          paymentSteps == 1 && installment?.quantity == 1 && `${clientFirstName}, pague o valor de ${installmentValue} no pix`
         }
 
         {
-          paymentSteps == 1 && installment?.quantity! > 1 && `${clientInfo.name}, pague a entrada de ${installmentValue} no pix`
+          paymentSteps == 1 && installment?.quantity! > 1 && `${clientFirstName}, pague a entrada de ${installmentValue} no pix`
         }
 
         {
-          paymentSteps > 1 && paymentSteps != installment?.quantity && `${clientInfo.name}, pague a ${paymentSteps}ª parcela em 1x no cartão`
+          paymentSteps > 1 && paymentSteps != installment?.quantity && `${clientFirstName}, pague a ${paymentSteps}ª parcela em 1x no cartão`
         }
 
         {
-          paymentSteps == installment?.quantity && installment.quantity != 1 && `${clientInfo.name}, pague o restante em 1x no cartão`
+          paymentSteps == installment?.quantity && installment.quantity != 1 && `${clientFirstName}, pague o restante em 1x no cartão`
         }
       </h2>
 
@@ -47,7 +97,7 @@ export default function Payment() {
       }
 
       <Deadline
-        time={pixInfo.expiresIn}
+        time={pixInfo?.expiresIn}
       />
       <PaymentProgress />
 
@@ -65,7 +115,7 @@ export default function Payment() {
 
       <section className={styles.paymentId}>
         <span>Identificador:</span>
-        <strong>{pixInfo.identifier}</strong>
+        <strong>{pixInfo?.identifier}</strong>
       </section>
     </main>
   )
