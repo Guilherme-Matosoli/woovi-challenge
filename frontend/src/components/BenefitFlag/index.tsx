2bf5c9cc4f4fb8fe61bb2f3d46e8d97a8ff25c1c
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-14 23:28:25 -0300

    :sparkles: feat(components/BenefitFlag): Add `abbr` element to `BenefitFlag` component for accessibility

--- frontend/src/components/BenefitFlag/index.tsx
+++ frontend/src/components/BenefitFlag/index.tsx
@@ -7,9 +7,11 @@ interface BenefitFlagProps {
 export function BenefitFlag({ text }: BenefitFlagProps) {
   return (
     <div className={styles.container}>
+      <abbr title={text}>
       <span className={styles.text}>
         {text}
       </span>
+      </abbr>
     </div>
   )
 };
