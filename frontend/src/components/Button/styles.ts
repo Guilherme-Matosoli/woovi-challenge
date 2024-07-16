478e9cebbe0330996f4dd4c50c06bf64b3939107
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-16 17:20:39 -0300

    :lipstick: style(components/Button): Update button styles to use CSS variables and adjust gap size

--- frontend/src/components/Button/styles.ts
+++ frontend/src/components/Button/styles.ts
@@ -4,17 +4,17 @@ export const Container = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 10px;
+  gap: 5px;
 
   width: 100%;
 
-  font-family: 'Nunito', sans-serif;
+  font-family: var(--primary-font);
   font-size: 18px;
   font-weight: 600;
   color: white;
 
   border: none;
-  background-color: #133A6F;
+  background-color: var(--dark-blue);
   border-radius: 8px;
 
   padding: 7px;
